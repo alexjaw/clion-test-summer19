1a88ca3ca7ad5d0203da7e61807825f4b73768b7
Alexander Jaworowski <alexander@jaworowski.se>
2019-08-06 16:54:42 +0200

    Add: - TEST_CASE..SECTION so that we have common setup, compare setup() teardown() in other frameworks

--- test/LedDriverTest.cpp
+++ test/LedDriverTest.cpp
@@ -6,41 +6,42 @@ extern "C" {
 #include "LedDriver.h"
 }
 
-TEST_CASE("LedsAreOffAfterCreate"){
-    uint16_t virtualLeds = 0xffff;
-    LedDriver_Create(&virtualLeds);
-    REQUIRE(virtualLeds == 0);
+TEST_CASE("LedDriver init tests"){
+    uint16_t virtualLeds;
+
+    SECTION("LedsAreOffAfterCreate"){
+        virtualLeds = 0xffff;
+        LedDriver_Create(&virtualLeds);
+        REQUIRE(virtualLeds == 0);
+    }
 }
 
-TEST_CASE("TurnOnLedOne"){
+TEST_CASE("LedDriver tests"){
     uint16_t virtualLeds;
     LedDriver_Create(&virtualLeds);
-    LedDriver_TurnOn(1);
-    REQUIRE(virtualLeds == 1);
-}
 
-TEST_CASE("TurnOffLedOne"){
-    uint16_t virtualLeds;
-    LedDriver_Create(&virtualLeds);
-    LedDriver_TurnOn(1);
-    REQUIRE(virtualLeds == 1);
-    LedDriver_TurnOff(1);
-    REQUIRE(virtualLeds == 0);
-}
+    SECTION("TurnOnLedOne"){
+        LedDriver_TurnOn(1);
+        REQUIRE(virtualLeds == 1);
+    }
 
-TEST_CASE("TurnOnMultipleLeds"){
-    uint16_t virtualLeds;
-    LedDriver_Create(&virtualLeds);
-    LedDriver_TurnOn(9);
-    LedDriver_TurnOn(8);
-    REQUIRE(virtualLeds == 0x180);
-}
+    SECTION("TurnOffLedOne"){
+        LedDriver_TurnOn(1);
+        REQUIRE(virtualLeds == 1);
+        LedDriver_TurnOff(1);
+        REQUIRE(virtualLeds == 0);
+    }
 
-TEST_CASE("TurnOffAnyLed"){
-    uint16_t virtualLeds;
-    LedDriver_Create(&virtualLeds);
-    LedDriver_TurnAllOn();
-    REQUIRE(0xffff == virtualLeds);
-    LedDriver_TurnOff(8);
-    REQUIRE(0xff7f == virtualLeds);
+    SECTION("TurnOnMultipleLeds"){
+        LedDriver_TurnOn(9);
+        LedDriver_TurnOn(8);
+        REQUIRE(virtualLeds == 0x180);
+    }
+
+    SECTION("TurnOffAnyLed"){
+        LedDriver_TurnAllOn();
+        REQUIRE(0xffff == virtualLeds);
+        LedDriver_TurnOff(8);
+        REQUIRE(0xff7f == virtualLeds);
+    }
 }
