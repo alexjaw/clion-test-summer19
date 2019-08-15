ea74ddac1eea6cfb25b32ac779ea1f6beb01cbad
Alexander Jaworowski <alexander@jaworowski.se>
2019-08-15 16:57:13 +0200

    Add test ScheduleOnEverydayNotTimeYet in LightSchedulerTest

--- test/LightSchedulerTest.cpp
+++ test/LightSchedulerTest.cpp
@@ -24,4 +24,13 @@ TEST_CASE("LightController") {
         REQUIRE(LIGHT_ID_UNKNOWN == LightControllerSpy_GetLastId());
         REQUIRE(LIGHT_STATE_UNKNOWN == LightControllerSpy_GetLastState());
     }
+
+    SECTION("ScheduleOnEverydayNotTimeYet"){
+        LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
+        FakeTimeService_SetDay(MONDAY);
+        FakeTimeService_SetMinute(1199);
+        LightScheduler_WakeUp();
+        REQUIRE(LIGHT_ID_UNKNOWN == LightControllerSpy_GetLastId());
+        REQUIRE(LIGHT_STATE_UNKNOWN == LightControllerSpy_GetLastState());
+    }
 }
