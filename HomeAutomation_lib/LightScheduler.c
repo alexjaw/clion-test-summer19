c9be33311116a00720914802b66a23c945ebd68e
Alexander Jaworowski <alexander@jaworowski.se>
2019-09-06 11:05:17 +0200

    Add code changes in chapter 8 - Refactor to Remove Duplications - Refactor for Responsibility

--- HomeAutomation_lib/LightScheduler.c
+++ HomeAutomation_lib/LightScheduler.c
@@ -20,35 +20,43 @@ static ScheduledLightEvent scheduledEvent;
 
 static enum {UNUSED=-1, TURN_OFF=0, TURN_ON};
 
+static void scheduleEvent(int id, Day day, int minuteOfDay, int event){
+    scheduledEvent.id = id;
+    scheduledEvent.minuteOfDay = minuteOfDay;
+    scheduledEvent.event = event;
+}
+
+static void operateLight(ScheduledLightEvent * lightEvent){
+    if(lightEvent->event == TURN_ON) {
+        LightController_On(lightEvent->id);
+    }else if(lightEvent->event == TURN_OFF){
+        LightController_Off(lightEvent->id);
+    }
+}
+
+static void processEventDueTime(Time * time, ScheduledLightEvent * lightEvent){
+    if(lightEvent->id == UNUSED){ return; }
+
+    if(lightEvent->minuteOfDay != time->minuteOfDay){ return; }
+
+    operateLight(lightEvent);
+}
+
 void LightScheduler_Create(void){
     //scheduledEvent.id = UNUSED;
 }
 
 void LightScheduler_ScheduleTurnOn(int id, Day day, int minuteOfDay){
-    scheduledEvent.id = id;
-    scheduledEvent.minuteOfDay = minuteOfDay;
-    scheduledEvent.event = TURN_ON;
+    scheduleEvent(id, minuteOfDay, minuteOfDay, TURN_ON);
 }
 
 void LightScheduler_ScheduleTurnOff(int id, Day day, int minuteOfDay){
-    scheduledEvent.id = id;
-    scheduledEvent.minuteOfDay = minuteOfDay;
-    scheduledEvent.event = TURN_OFF;
+    scheduleEvent(id, minuteOfDay, minuteOfDay, TURN_OFF);
 }
 
-void LightScheduler_WakeUp(void){
+void LightScheduler_WakeUp(void) {
     Time time;
     TimeService_GetTime(&time);
 
-    if(scheduledEvent.id == UNUSED){
-        return;
-    }if(scheduledEvent.minuteOfDay != time.minuteOfDay){
-        return;
-    }
-
-    if(scheduledEvent.event == TURN_ON) {
-        LightController_On(scheduledEvent.id);
-    }else if(scheduledEvent.event == TURN_OFF){
-        LightController_Off(scheduledEvent.id);
-    }
+    processEventDueTime(&time, &scheduledEvent);
 }
